1e4e23390f1c4417c7523a43b9cb3ef4a67ffdd1
Arshad <mohammedarshad183@gmail.com>
2022-05-26 17:05:24 +0530

    Refactor: Ability to Add multiple person to Address Book

--- AddressBook/AddressBook/AddressBookMain.cs
+++ AddressBook/AddressBook/AddressBookMain.cs
@@ -11,16 +11,46 @@ namespace AddressBook
         static void Main(string[] args)
         {
             AddressBook addressbook = new AddressBook();
-            addressbook.DisplayContacts();
-            addressbook.AddContact();
-            Console.Write("Enter first name to Edit Record :  ");
-            string first = Console.ReadLine();
-            addressbook.EditContact(first);
-            Console.Write("Enter first name to Delete Record :  ");
-            string del = Console.ReadLine();
-            addressbook.DeleteContact(del);
-            Console.ReadLine();
 
+            bool userInput = true;
+
+            while (userInput)
+            {
+                Console.WriteLine("To SEE Contacts in  AddressBook   ----- press 1");
+                Console.WriteLine("To ADD Contact in Address Book    ----- press 2");
+                Console.WriteLine("To Edit Existing Contact          ----- press 3");
+                Console.WriteLine("To DELETE Existing Contact        ----- press 4");
+                Console.WriteLine("To EXIT                           ----- press x");
+
+
+                Console.Write("\n######    Select any option above  #######   --:  ");
+                string input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        addressbook.DisplayContacts();
+                        break;
+                    case "2":
+                        addressbook.AddContact();
+                        break;
+                    case "3":
+                        Console.Write("Enter the fist name to fetch the record :  ");
+                        string first = Console.ReadLine();
+                        addressbook.EditContact(first);
+                        break;
+                    case "4":
+                        Console.Write("Enter the fist name to fetch the record :  ");
+                        string del = Console.ReadLine();
+                        addressbook.DeleteContact(del);
+                        break;
+                    case "X":
+                        userInput = false;
+                        break;
+                    case "x":
+                        userInput = false;
+                        break;
+                }
+            }
 
         }
     }
